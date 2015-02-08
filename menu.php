31ea1e8485f5bd6e37788f16253874d8fabf1fce
CN91 <c.nibbering@student.avans.nl>
2015-02-08 19:50:27 +0100

    Extra css additions
    CSS toegevoegd, design gelijk getrokken met design-opzet

--- menu.php
+++ menu.php
@@ -1,15 +1,19 @@
 <nav>   
 	<ul id="menu">
 		<li class="top"><a href="index.php">Home</a></li>
+			<li class="top"><a href="#">Blog</a></li>
 		<li class="top">
-			<a href="#">Product Overzicht</a>
-			<ul>
+			<a href="#">Producten</a>
+			<ul class="submenu">
 				<li class="top"><a href="templates/detail.php">Detail X</a></li> 
 			</ul>
 		</li> 
-		<li class="top"><a href="#">Over BierHier</a></li>
 		<li class="top"><a href="#">Zoek</a></li> 
-		<li class="top"><a href="#">Accountgegevens</a></li> 
-		<li class="top"><a href="#">Blog</a></li>
+		<li class="top"><a href="#">Over ons</a>
+			<ul class="submenu">
+				<li class="top"><a href="#"> Contact </a> </li>
+			</ul>
+		</li>
+
 	</ul>
 </nav>
