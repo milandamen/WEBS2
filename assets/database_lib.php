07999dd531703802065e0d35181e6904b5573d63
milandamen <mdamen3@student.avans.nl>
2015-02-11 20:33:40 +0100

    Appended onto database_lib and started sessions

--- assets/database_lib.php
+++ assets/database_lib.php
@@ -3,34 +3,51 @@
 class db {
 
 	public $database;
+	
+	function __construct() {
+		$this->dbConnect();
+	}
 
-function __construct() {
-	$this->dbConnect();
-	echo 'test';
-}
+	public function dbConnect() {
+		global $database;
+		try {
+			$database = new PDO('mysql:host=159.253.0.111;dbname=corintn78_bier','corintn78_bier','Nan9pLgP');
+		} catch(PDOException $e){
+			echo 'Database error: '.$e->getMessage().'<br/>';
+		}
+	}
 
-
-public function dbConnect(){
-  	try{
-  	 	$database = new PDO('mysql:host=159.253.0.111;dbname=corintn78_bier','corintn78_bier','Nan9pLgP');
-	} catch(PDOException $e){
-		echo $e->getMessage().'<br/>';
+	public function dbClose() {
+		global $database;
+		$database = null;
+	}
+	
+	public function execQuery($query, $params = array(), $bindonvalue = false) {
+		global $database;
+		$stmt = $database->prepare($query);
+		
+		foreach ($params as $key => $value) {
+			$stmt->bindParam($key, $value);
+		}
+		
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		$stmt = null;
+		return $result;
+	}
+	
+	public function rowCountFromQuery($query, $params = array()) {
+		global $database;
+		$stmt = $database->prepare($query);
+		
+		foreach ($params as $key => $value) {
+			$stmt->bindParam($key, $value);
+		}
+		
+		$stmt->execute();
+		$rowcount = $stmt->rowCount();
+		$stmt = null;
+		return $rowcount;
 	}
 }
-
-public function dbClose(){
-
-}
-
-public function execQuery(){
-
-
-
-
-}
-
-}
-
-
-
 ?>
--- assets/mini.php
+++ assets/mini.php
@@ -1,9 +1,11 @@
-
 <?php 
 include_once 'database_lib.php';
 
 $db = new db();
+//$result = $db->execQuery('SELECT * FROM country');
+//$result = $db->execQuery('INSERT INTO country (name) VALUES (:name)', array(':name' => 'Nederland'));
+$result = $db->rowCountFromQuery('SELECT * FROM country');
+$db->dbClose();
 
-
-
- ?>
+var_dump($result);
+?>
