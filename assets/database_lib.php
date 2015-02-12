0410100ac7f48e254668e7c75d412810c3b5a08b
milandamen <mdamen3@student.avans.nl>
2015-02-12 11:08:07 +0100

    database_lib almost done & session

--- assets/database_lib.php
+++ assets/database_lib.php
@@ -4,27 +4,50 @@ class db {
 
 	public $database;
 	
-	function __construct() {
+	// Smart statements
+	private $usesmartstmt;
+	private $lastquery;
+	private $laststmt;
+	
+	// TODO __destruct()
+	
+	function __construct($smartstmt = false) {
+		$this->usesmartstmt = $smartstmt;
 		$this->dbConnect();
+	}
+	
+	function __destruct() {
+		$this->dbClose();
 	}
 
 	public function dbConnect() {
-		global $database;
 		try {
-			$database = new PDO('mysql:host=159.253.0.111;dbname=corintn78_bier','corintn78_bier','Nan9pLgP');
+			$this->database = new PDO('mysql:host=159.253.0.111;dbname=corintn78_bier','corintn78_bier','Nan9pLgP');
 		} catch(PDOException $e){
 			echo 'Database error: '.$e->getMessage().'<br/>';
 		}
 	}
 
 	public function dbClose() {
-		global $database;
-		$database = null;
+		$this->closeLastStatement();
+		$this->database = null;
 	}
 	
-	public function execQuery($query, $params = array(), $bindonvalue = false) {
-		global $database;
-		$stmt = $database->prepare($query);
+	public function closeLastStatement() {
+		$this->lastquery = null;
+		$this->laststmt = null;
+	}
+	
+	public function execQuery($query, $params = array()) {
+		$stmt = null;
+		if ($this->usesmartstmt && $this->isLastQuery($query)) {
+			$stmt = $this->laststmt;
+			echo 'last stmt';
+		} else {
+			$this->closeLastStatement();
+			$stmt = $this->database->prepare($query);
+			echo 'new stmt';
+		}
 		
 		foreach ($params as $key => $value) {
 			$stmt->bindParam($key, $value);
@@ -32,13 +55,25 @@ class db {
 		
 		$stmt->execute();
 		$result = $stmt->fetchAll();
-		$stmt = null;
+		if ($this->usesmartstmt) {
+			$this->lastquery = $query;
+			$this->laststmt = $stmt;
+		} else {
+			$stmt = null;
+		}
 		return $result;
 	}
 	
 	public function rowCountFromQuery($query, $params = array()) {
-		global $database;
-		$stmt = $database->prepare($query);
+		$stmt = null;
+		if ($this->usesmartstmt && $this->isLastQuery($query)) {
+			$stmt = $this->laststmt;
+			echo 'last stmt';
+		} else {
+			$this->closeLastStatement();
+			$stmt = $this->database->prepare($query);
+			echo 'new stmt';
+		}
 		
 		foreach ($params as $key => $value) {
 			$stmt->bindParam($key, $value);
@@ -46,8 +81,21 @@ class db {
 		
 		$stmt->execute();
 		$rowcount = $stmt->rowCount();
-		$stmt = null;
+		if ($this->usesmartstmt) {
+			$this->lastquery = $query;
+			$this->laststmt = $stmt;
+		} else {
+			$stmt = null;
+		}
 		return $rowcount;
+	}
+	
+	public function setUseSmartStatement($bool) {
+		$this->usesmartstmt = $bool;
+	}
+	
+	private function isLastQuery($query) {
+		return $this->lastquery === $query;
 	}
 }
 ?>
