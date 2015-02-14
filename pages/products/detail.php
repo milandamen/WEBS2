75a8a1872442c498255ddf312a6db61bc6417544
milandamen <mdamen3@student.avans.nl>
2015-02-14 16:50:47 +0100

    product detail almost done, will move query to seperate file

--- pages/products/detail.php
+++ pages/products/detail.php
@@ -1,73 +1,95 @@
 <?php 
+include_once '../../assets/session_lib.php';
 include_once '../../assets/database_lib.php';
 
+$product;
 
+if (isset($_GET['id'])) {
+	$db = new db();
+	
+	$result = $db->execQuery('
+			SELECT 
+				b.name AS name,
+				br.name AS brand,
+				s.name AS sort,
+				b.content AS content,
+				b.percentage AS percentage,
+				w.name AS wrapping,
+				brw.name AS brewery,
+				c.name AS country,
+				b.description AS description 
+			FROM beer AS b 
+			LEFT JOIN brand AS br 
+				ON br.id = b.brand_id 
+			LEFT JOIN sort AS s 
+				ON s.id = b.sort_id 
+			LEFT JOIN wrapping AS w 
+				ON w.id = b.wrapping_id 
+			LEFT JOIN brewery AS brw 
+				ON brw.id = b.brewery_id 
+			LEFT JOIN country AS c 
+				ON c.id = brw.country_id 
+			WHERE b.id = :id', 
+			array(':id' => $_GET['id']), 
+			db::FETCH_OBJ);
+	
+	$db->dbClose();
+	
+	if (is_array($result) && count($result) == 1) {
+		$product = $result[0];
+	} else {
+		die('ERROR: Recieved invalid result from database!');
+	}
+} else {
+	die('ERROR: GET request invalid!');
+}
 ?>
 <?php include("../../header.php"); ?>
 
 <div id="content">
-
-	<?php  
-
-	$productname = 'La Chouffe';
-	$sort = 'zwaar blond';
-	$contains = 0.75;
-	$alcohol = 8;
-	$wrapping = 'Fles';
-	$brewery = 'Brasserie d\'Achouffe';
-	$info = '
-			Wil je iemand op originele manier verrassen? Dat kan met deze fles La
-			Chouffe (0,75 liter 8% vol). Dit zwaar blond speciaalbier uit Belgie
-			heeft een fruitige geur van perzik en frisse kruidige geur van koriander.
-			Het is een bier waar bitter, zoet en fruitsmaken elkaar afwisselen; een
-			ware smaakexplosie.';
-
-	?>
-	<h6> <?php echo $productname ?> </h3>
-<div class="details">
-
-	<img class="prod_detailimg" src="pages/products/img/<?php echo $productname ?>/<?php echo $productname ?>.png" />
-	<h4> Specificaties </h4>
-	<p> 
-		<table>
-			<tr>
-				<td> Soort </td>
-				<td> <?php echo $sort; ?></td>
-			</tr>
-			<tr>
-				<td> Inhoud  </td>
-				<td> <?php echo $contains; ?></td>
-			</tr>
-			<tr>
-				<td> Alcohol %  </td>
-				<td> <?php echo $alcohol; ?></td>
-			</tr>
-			<tr>
-				<td> Verpakking </td>
-				<td> <?php echo $wrapping; ?></td>
-			</tr>
-			<tr>
-				<td> Brouwerij  </td>
-				<td> <?php echo $brewery; ?></td>
-			</tr>
-
-		</table>
-	</p>
-
-
-	<br />
-	<br />
-	<br />
-	<br />
-	<br />
-
-	<h4> Omschrijving </h4>
-	<p> <?php echo $info; ?> </p>    
-</div>
-
-<input class="buttondetail" type="button" value="Bestellen" />
-<input class="buttondetail" type="button" value="Verlanglijstje" />
-
+	<h6><?php echo $product->name ?></h3>
+	<div class="details">
+		<img class="prod_detailimg" src="pages/products/img/<?php echo $product->name; ?>/<?php echo $product->name; ?>.png" />
+		<h4>Specificaties</h4>
+		<p> 
+			<table>
+				<tr>
+					<td>Merk</td>
+					<td><?php echo $product->brand; ?></td>
+				</tr>
+				<tr>
+					<td>Soort</td>
+					<td><?php echo $product->sort; ?></td>
+				</tr>
+				<tr>
+					<td>Inhoud</td>
+					<td><?php echo $product->content; ?></td>
+				</tr>
+				<tr>
+					<td>Alcohol</td>
+					<td><?php echo $product->percentage; ?></td>
+				</tr>
+				<tr>
+					<td>Verpakking</td>
+					<td><?php echo $product->wrapping; ?></td>
+				</tr>
+				<tr>
+					<td>Brouwerij</td>
+					<td><?php echo $product->brewery; ?></td>
+				</tr>
+				<tr>
+					<td>Land</td>
+					<td><?php echo $product->country; ?></td>
+				</tr>
+			</table>
+		</p>
+		
+		<h4>Omschrijving</h4>
+		<p><?php echo $product->description; ?></p>    
+	</div>
+	
+	<input class="buttondetail" type="button" value="Bestellen" />
+	<input class="buttondetail" type="button" value="Verlanglijstje" />
 </div>
 
 <?php include('../../sidebar.php'); ?>
